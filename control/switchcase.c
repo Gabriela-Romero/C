0c0780353444dcd3bcc1f5ffda6c420a481212f8
Gabriela Romero Ortega <63374375+Gabriela-Romero@users.noreply.github.com>
2020-11-09 12:10:07 -0600

    Ejercicio switch
    Se realizaron los cambios para cumplir con el ejercicio requerido de switch

--- control/switchcase.c
+++ control/switchcase.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 int main()
 {
 	int opcion;
-	printf("Elige una opción:");
+	float c, b, a;
+	printf ("Bienvenido, este programa te ayudara a hacer operaciones con dos numeros enteros o con decimales. Antes de seleccionar la operacion que deseas hacer, dame el primer numero:");
+    scanf("%f", &a);
+    printf ("Dame el segundo numero:");
+    scanf("%f", &b);
+	printf("Elige una opcion:\n");
 	printf("\t 1.- Suma:\n");
 	printf("\t 2.- Resta:\n");
-	printf("\t 6.- Salir:\n");
+	printf("\t 3.- Multiplicacion:\n");
+	printf("\t 4.- Division:\n");
+	printf("\t 5.- Potencia:\n");
+	printf("\t 6.- Raiz:\n");
+	printf("\t 7.- Salir:\n");
 	scanf("%d",&opcion);
 	switch(opcion)
 	{
@@ -14,10 +24,27 @@ int main()
 		c= a +b ;
 		break;
 	case 2: 
+	    c= a -b; 
 		break;
+	case 3: 
+	    c=a*b;
+		break;
+	case 4: 
+	    c=a/b; 
+		break;	
+	case 5: 
+	   c = pow(b, a); 
+		break;
+	case 6: 
+	   c=sqrt(a); 
+	   c=sqrt(b);
+		break;	
+	case 7: 
+	    return 0; 
+		break;			
 	default:
-		printf("No está puesta correctamente la unidad\n");
+		printf("No esta puesta correctamente la unidad\n");
 		exit(0);
 	}
-	printf("\nLas %f pulgadas son %f centímetros\n", pul, cm);
+	printf("\nEl resultado es %f \n", c);
 } 
