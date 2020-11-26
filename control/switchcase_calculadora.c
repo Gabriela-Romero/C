e31d8602bcc56245883ae4b5cf1f81de391d5b66
Gabriela Romero Ortega <63374375+Gabriela-Romero@users.noreply.github.com>
2020-11-26 15:55:37 -0600

    MODIFICACION TAREA
    Se hicieron los cambiios para cumplir con los requerimientos de la tarea, pero no se ha detectado como olucionar el error que hace que no se muestren los reultados y solo nos salga 0

--- control/switchcase_calculadora.c
+++ control/switchcase_calculadora.c
@@ -4,46 +4,45 @@
 
 int main()
 {
-	//Declarando variables
-	int opcion;
-	float n1, n2, resultado;
-	
-	printf("Elige una opcion:\n");
+	int opcion, i;
+	int fact = 1;
+	float resultado, n1, n2;
+	do{
+	printf("\n Elige una opcion:\n");
 	printf("\t 1.- Suma\n");
 	printf("\t 2.- Resta\n");
 	printf("\t 3.- Division\n");
 	printf("\t 4.- Multiplicacion\n");
 	printf("\t 5.- Potencia\n");
 	printf("\t 6.- Raiz cuadrada\n");
-	printf("\t 7.- Salir\n");
+	printf("\t 7.- Factorial\n");
+	printf("\t 8.- Salir\n");
 	scanf("%d",&opcion);
 	system("cls");
 	
-	if(opcion==6){
-		printf("\nDame el primer valor: ");
-		scanf("%f", &n1);
-	}
-	else if(opcion>=7){
-	}
-	else{
-		printf("\nDame el primer valor: ");
-		scanf("%f", &n1);
-		printf("\nDame el segundo valor: ");
-		scanf("%f", &n2);
-	}
-
-	//Estructura de control de selección multiple
 	switch(opcion)
 	{
 	case 1:
+		printf("Introduce el primer numero:");
+		scanf("%d",&n1);
+		printf("Introduce el segundo numero:");
+		scanf("%d",&n2);
 		resultado = n1 + n2;
 		printf("%f", resultado);
 		break;
 	case 2:
+		printf("Introduce el primer numero:");
+		scanf("%d",&n1);
+		printf("Introduce el segundo numero:");
+		scanf("%d",&n2);
 		resultado = n1 - n2;
 		printf("%f", resultado);
 		break;
 	case 3: 
+	printf("Introduce el primer numero:");
+		scanf("%d",&n1);
+		printf("Introduce el segundo numero:");
+		scanf("%d",&n2);
 		if(n2==0)
 		{
 			printf("La operacion entre 0 no esta definida");
@@ -54,14 +53,24 @@ int main()
 		}
 		break;
 	case 4: 
+	printf("Introduce el primer numero:");
+		scanf("%d",&n1);
+		printf("Introduce el segundo numero:");
+		scanf("%d",&n2);
 		resultado = n1 * n2;
 		printf("%f", resultado);
 		break;
-	case 5: 		
+	case 5: 
+		printf("Introduce el primer numero:");
+		scanf("%d",&n1);
+		printf("Introduce el segundo numero:");
+		scanf("%d",&n2);
 		resultado = pow(n1,n2);
 		printf("%f", resultado);
 		break;
 	case 6: 
+	printf("Introduce el numero:");
+		scanf("%d",&n1);
 		if(n1<0){
 				printf("No esta definida");
 		}
@@ -70,8 +79,26 @@ int main()
 			printf("%f", resultado);
 		}
 		break;
+	case 7: 
+	printf("Introduce el numero:");
+		scanf("%d",&n1);
+		if(n1<0) {fact = 0;}
+		else if (n1 == 0 ) {fact = 0;}
+		else {
+		for (i= 1; i <= n1; i++){
+			fact = fact*i;
+			}
+			}
+	printf("%f", resultado);
+		break;
+	case 8:
+	return 0;
+	break;
 	default:
 		printf("Salir");
 		exit(0);
-	}	//Sale break
-} 
+		}	
+	} while (opcion<=8);
+		}	
+		
+
